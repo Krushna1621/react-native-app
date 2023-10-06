4f476d764acbaa7378b97faf3abbbfd6c7932083
Krushna1621 <shindekrushna825@gmail.com>
2023-10-06 11:02:34 +0530

    Its  All About Alert

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { View, Text, Image, Button, Modal, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  Modal,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   const [isModalVisible, setIsMoadalVisble] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ActivityIndicator></ActivityIndicator>
-      <ActivityIndicator size="large"></ActivityIndicator>
-      <ActivityIndicator size="large" color="midnightblue"></ActivityIndicator>
-      <ActivityIndicator size="large" color="midnightblue" animating={false}></ActivityIndicator>
+      <Button
+        title="Alert"
+        onPress={() => Alert.alert("Invalid data")}
+      ></Button>
+      <Button
+        title="Alert 2"
+        onPress={()=>Alert.alert("Invalid data!","DOB incorrect")}
+      ></Button>
+       <Button
+        title="Alert 3"
+        onPress={()=>Alert.alert("Invalid data!","DOB incorrect",[{
+            text:'Cancel', 
+            onPress:()=>console.log("Cancel pressed")
+        },
+        {
+            text:'Ok', 
+            onPress:()=>console.log("Cancel pressed")
+        }])}
+      ></Button>
     </View>
   );
 }
