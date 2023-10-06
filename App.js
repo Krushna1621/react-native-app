712473f54a5bb74f020e7d8804bb9fa6e7b140f7
Krushna1621 <shindekrushna825@gmail.com>
2023-10-06 12:26:39 +0530

    multiple style or box model add succefully

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import {View,Text,StyleSheet} from 'react-native'
 export default function App(){
     return(
         <View style={style.container}>
-            <View style={style.lightblueBox}>
-                <Text>Lightblue Box</Text>
+            <View style={style.darkMode}>
+                <Text style={style.darkText}>Style Inheritance</Text>
             </View>
-            <View style={style.lightgreenBox}>
+            <View style={[style.lightblueBg,style.Box]}>
+                <Text style={{borderRadius:5,backgroundColor:"red"}}>Lightblue Box</Text>
+            </View>
+            <View style={style.lightgreenBg}>
                 <Text>Lightgreen Box</Text>
-            </View>
+            </View> 
         </View>
         
     )
@@ -16,13 +19,27 @@ export default function App(){
 
 const style=StyleSheet.create({
     container:{flex:1,backgroundColor:"red",padding:60},
-    lightblueBox:{
-        backgroundColor:"lightblue",
-        width:100,
-        height:100,
-        padding:10
+    darkMode:{
+        backgroundColor:"black"
     },
-    lightgreenBox:{
+    darkText:{
+        color:"white"
+    },
+    Box:{width:100,
+        height:100,
+        // padding:10,
+        paddingHorizontal:20,
+        paddingVertical:20,
+        marginVertical:10,
+        backgroundColor:"pink",
+        borderRadius:10
+    }
+    ,
+    lightblueBg:{
+        backgroundColor:"lightblue",
+        
+    },
+    lightgreenBg:{
         backgroundColor:"lightgreen",
         width:100,
         height:100,
