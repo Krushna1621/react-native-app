4b1bb04b853ab0cb85961423052f6dc0d0266317
Krushna1621 <shindekrushna825@gmail.com>
2023-10-06 10:42:55 +0530

    Its all About StatusBar code

--- App.js
+++ App.js
@@ -1,35 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { View, Text, Image, Button, Modal } from "react-native";
+import { View, Text, Image, Button, Modal, ActivityIndicator } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   const [isModalVisible, setIsMoadalVisble] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-        <StatusBar backgroundColor="lightgreen" barStyle="dark-content" hidden></StatusBar>
-      {/* <Text>Krushna</Text> */}
-      {/* <Text><Text style={{color:"red"}}>Krushnakant</Text> Shinde</Text> */}
-      {/* <Image source={logoImg} style={{width:300,height:300}} /> */}
-      {/* <Button
-        title="Press"
-        onPress={() => setIsMoadalVisble(true)}
-        color="midnightblue"
-      />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsMoadalVisble(false)}
-        animationType="slide"
-        presentationStyle="pageSheet"
-      >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Modal Content</Text>
-          <Button
-            title="Close"
-            color="midnightblue"
-            onPress={() => setIsMoadalVisble(false)}
-          ></Button>
-        </View>
-      </Modal> */}
+      <ActivityIndicator></ActivityIndicator>
+      <ActivityIndicator size="large"></ActivityIndicator>
+      <ActivityIndicator size="large" color="midnightblue"></ActivityIndicator>
+      <ActivityIndicator size="large" color="midnightblue" animating={false}></ActivityIndicator>
     </View>
   );
 }
