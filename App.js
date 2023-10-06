7018720669bae9648c7bbbdcc1bdf7126d2d37b8
Krushna1621 <shindekrushna825@gmail.com>
2023-10-06 11:40:15 +0530

    stylesheet API advanced

--- App.js
+++ App.js
@@ -3,12 +3,29 @@ import {View,Text,StyleSheet} from 'react-native'
 export default function App(){
     return(
         <View style={style.container}>
-            <Text style={style.title}>StyleSheet API</Text>
+            <View style={style.lightblueBox}>
+                <Text>Lightblue Box</Text>
+            </View>
+            <View style={style.lightgreenBox}>
+                <Text>Lightgreen Box</Text>
+            </View>
         </View>
+        
     )
 }
 
 const style=StyleSheet.create({
     container:{flex:1,backgroundColor:"red",padding:60},
-    title:{color:"yellow"} 
+    lightblueBox:{
+        backgroundColor:"lightblue",
+        width:100,
+        height:100,
+        padding:10
+    },
+    lightgreenBox:{
+        backgroundColor:"lightgreen",
+        width:100,
+        height:100,
+        padding:10
+    }
 })
